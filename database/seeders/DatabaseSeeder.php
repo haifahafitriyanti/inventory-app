daf72bfa74230b63550244bfeaf9c6dde985216f
Haifa Hafitriyanti <haifahafitriyanti@gmail.com>
2026-04-21 13:19:55 +0700

    Laravel uploas to github

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run()
+    {
+        $this->call(CategorySeeder::class);
+        // Tambahkan 50 produk acuan
+        \App\Models\Product::factory(50)->create();
+    }
+}
